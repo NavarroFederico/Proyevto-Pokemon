ccf667be8f5a5a65039b0e367d66610168c43221
Federico Navarro <navarrofederico14@gmail.com>
2022-10-02 08:28:49 -0300

    services retrofit

--- app/src/main/java/com/example/proyectomvvmpokemon/services/RetrofitClient.kt
+++ app/src/main/java/com/example/proyectomvvmpokemon/services/RetrofitClient.kt
@@ -1,4 +1,39 @@
 package com.example.proyectomvvmpokemon.services
 
+import com.example.proyectomvvmpokemon.BuildConfig
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
 object RetrofitClient {
+    private var retrofit: Retrofit? = null
+    private const val BASE_URL = "https://raw.githubusercontent.com/Biuni/PokemonGO-Pokedex/master"
+//aca pasamos la base url de la api cortamos "pokedex.json" para que cuando desde el GET de la interface se pegue con esta base
+
+    //hago el get para consumir el servicio
+    private val logger: OkHttpClient
+        get() {
+            val logging = HttpLoggingInterceptor()//nos sirve para debuggear la app para saber si nos esta llegando la respuesta
+            logging.level= HttpLoggingInterceptor.Level.BODY
+
+            val httpClient= OkHttpClient.Builder()
+                .connectTimeout(30,TimeUnit.SECONDS)
+                .readTimeout(30,TimeUnit.SECONDS)
+                .writeTimeout(30,TimeUnit.SECONDS)
+            //if(BuildConfig) httpClient.interceptors().add(logging)
+                return httpClient.build()
+        }
+    val getClient : Retrofit?
+    get() {
+        if(retrofit == null){
+            retrofit= Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(logger)
+                .build()
+        }
+        return retrofit
+    }
 }
--- app/src/main/java/com/example/proyectomvvmpokemon/services/WebService.kt
+++ app/src/main/java/com/example/proyectomvvmpokemon/services/WebService.kt
@@ -5,7 +5,7 @@ import retrofit2.Response
 import retrofit2.http.GET
 
 interface WebService {
-    @GET("")//aca va la api
+    @GET("pokedex.json")//aca va consumir el servicio de la url completa
 
     suspend fun getPokemos(): Response<PokemonResponse>
 
